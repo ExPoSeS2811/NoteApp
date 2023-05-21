e35279385386ce547fa77fea75dd2c49fb8cb84f
Gleb Rasskazov <glebunium146@gmail.com>
2023-05-21 09:37:14 +0300

    initial commit

---
+++ NoteApp/NoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by MacBook Pro 13 2019 on 5/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
